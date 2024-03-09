386f503645ac70c317c43aa98d01af2d14e41a2d
code-ga <tritranduc11@gmail.com>
2024-03-09 16:26:22 +0700

    update some waiting logic

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,40 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     loop {
         let job = main_rx.recv().await.unwrap();
+        if worker_rx.is_empty() {
+            let wait_time = rand::thread_rng().gen_range(1..5);
+            tokio::time::sleep(std::time::Duration::from_secs(wait_time)).await;
+            let mut pending_url = util::get_pending_urls(
+                &client,
+                num_of_threads + 5 - worker_rx.len(),
+                "".to_string(),
+            )
+            .await;
+            while !worker_rx.is_full() {
+                let pending_url = {
+                    let tmp = pending_url.pop();
+                    if tmp.is_none() {
+                        let wait_time = rand::thread_rng().gen_range(1..5);
+                        tokio::time::sleep(std::time::Duration::from_secs(wait_time)).await;
+                        pending_url = util::get_pending_urls(
+                            &client,
+                            num_of_threads + 5 - worker_rx.len(),
+                            "".to_string(),
+                        )
+                        .await;
+                        continue;
+                    }
+                    tmp.unwrap()
+                };
+                worker_tx
+                    .send(types::thread_message::ThreadMessage::Start(
+                        pending_url.to_string(),
+                        0,
+                    ))
+                    .await
+                    .unwrap();
+            }
+        }
         match job {
             ThreadMessage::Stop(id) => {
                 // spawn new worker and replace old
@@ -78,14 +112,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         let pending_url = {
                             let tmp = pending_urls.pop();
                             if tmp.is_none() {
-                                // tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
-                                pending_urls = util::get_pending_urls(
-                                    &client,
-                                    num_of_threads + 5 - worker_rx.len(),
-                                    url.clone(),
-                                )
-                                .await;
-                                continue;
+                                // let wait_time = rand::thread_rng().gen_range(1..5);
+                                // tokio::time::sleep(std::time::Duration::from_secs(wait_time)).await;
+                                // pending_urls = util::get_pending_urls(
+                                //     &client,
+                                //     num_of_threads + 5 - worker_rx.len(),
+                                //     url.clone(),
+                                // )
+                                // .await;
+                                // continue;
+                                break;
                             }
                             tmp.unwrap()
                         };
@@ -135,15 +171,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     let pending_url = {
                         let tmp = pending_urls.pop();
                         if tmp.is_none() {
-                            let wait_time = rand::thread_rng().gen_range(1..5);
-                            tokio::time::sleep(std::time::Duration::from_secs(wait_time)).await;
-                            pending_urls = util::get_pending_urls(
-                                &client,
-                                num_of_threads + 5 - worker_rx.len(),
-                                comic_url.clone(),
-                            )
-                            .await;
-                            continue;
+                            // let wait_time = rand::thread_rng().gen_range(1..5);
+                            // tokio::time::sleep(std::time::Duration::from_secs(wait_time)).await;
+                            // pending_urls = util::get_pending_urls(
+                            //     &client,
+                            //     num_of_threads + 5 - worker_rx.len(),
+                            //     comic_url.clone(),
+                            // )
+                            // .await;
+                            // continue;
+                            break;
                         }
                         tmp.unwrap()
                     };
