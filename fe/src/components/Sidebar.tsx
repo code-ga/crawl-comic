e9991e0f9714c4ac973417dfd6845470ad218821
code-ga <tritranduc11@gmail.com>
2024-03-15 20:15:23 +0700

    update

--- fe/src/components/Sidebar.tsx
+++ fe/src/components/Sidebar.tsx
@@ -22,7 +22,7 @@ export const SideBar = ({ app }: { app: AppApi }) => {
       <p>Side Bar</p>
       <span className="my-2 mt-4">Server Fetching Urls</span>
       <div className="text-sm text-left p-3">
-        {[...fetching].map((f) => (
+        {Array.from(fetching).map((f) => (
           <p
             key={f}
             className="p-2 bg-slate-900 my-3 break-words border rounded"
