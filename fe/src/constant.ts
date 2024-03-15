c3b8dbe02c6a5e5946f3f7c5ca9955c37f7824b6
code-ga <tritranduc11@gmail.com>
2024-03-15 20:08:06 +0700

    update be url

--- fe/src/constant.ts
+++ fe/src/constant.ts
@@ -1,2 +1,2 @@
-export const beUrl = "http://localhost:8080" //"https://ai-datalake.nz.io.vn/"
+export const beUrl = "https://ai-datalake.nz.io.vn/"
 export const cdnUrl = "https://ai-datalake.nz.io.vn/cdn"
