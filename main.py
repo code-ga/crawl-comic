1127e1a851318dba7538f148bb390e904f71cdc1
code-ga <tritranduc11@gmail.com>
2024-03-21 17:34:50 +0700

    add nettruyen parsing

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import aiohttp
 from dotenv import load_dotenv
 from prisma import Prisma
 
-from modules.blogtruyenmoi import parseComicHtmlPage
+from modules.blogtruyenmoi import parseComicHtmlPage as parseComicHtmlPage_blogtruyenmoi
+from modules.nettruyenff import parseComicHtmlPage as parseComicHtmlPage_nettruyenff
 
 
 load_dotenv(dotenv_path=".env")
@@ -14,31 +15,42 @@ async def main():
     await db.connect()
 
     while True:
-        comic_in_db = await db.comic.find_first(
-            where={"pythonFetchInfo": False}, order={"createdDate": "asc"}
-        )
+        comic_in_db = await db.comic.find_first(where={"pythonFetchInfo": False})
         if not comic_in_db:
             break
         async with aiohttp.ClientSession() as session:
-            async with session.get(comic_in_db.url) as response:
+            print(f"fetching url {comic_in_db.url} - {comic_in_db.id}")
+            async with session.get(
+                comic_in_db.url, headers={"User-Agent": "Mozilla/5.0"}
+            ) as response:
+                if response.status == 404 and "nettruyen" in comic_in_db.url:
+                    print("comic not found")
+                    await db.comic.update(
+                        where={"id": comic_in_db.id},
+                        data={"pythonFetchInfo": True},
+                    )
+                    continue
                 if response.status == 200:
                     print(f"parsing url {comic_in_db.url} - {comic_in_db.id}")
                     html = await response.text()
-                    if "Truyện đã bị xóa hoặc không tồn tại!" in html:
-                        print("comic not found")
-                        await db.comic.update(
-                            where={"id": comic_in_db.id},
-                            data={"pythonFetchInfo": True},
-                        )
-                        continue
-                    comic = parseComicHtmlPage(html, comic_in_db)
+                    comic = {}
+                    if "blogtruyenmoi" in comic_in_db.url:
+                        if "Truyện đã bị xóa hoặc không tồn tại!" in html:
+                            print("comic not found")
+                            await db.comic.update(
+                                where={"id": comic_in_db.id},
+                                data={"pythonFetchInfo": True},
+                            )
+                            continue
+                        comic = parseComicHtmlPage_blogtruyenmoi(html, comic_in_db)
+                    elif "nettruyen" in comic_in_db.url:
+                        comic = parseComicHtmlPage_nettruyenff(html, comic_in_db)
                     await db.comic.update(
                         where={"id": comic_in_db.id},
                         data=comic,
                     )
                     print("updated comic", comic_in_db.id)
-                    # await asyncio.sleep(1)
-
+                    await asyncio.sleep(1)
     await db.disconnect()
 
 
---
+++ modules/nettruyenff.py
@@ -0,0 +1,69 @@
+from util.parse import CustomBeautifulSoup
+from prisma.models import Comic
+from prisma import Json
+
+
+def parseComicHtmlPage(html: str, comic_in_db: Comic):
+    # Assuming you have the HTML content in a variable called html_content
+
+    soup = CustomBeautifulSoup(html, "html.parser")
+
+    result = {}
+    result["pythonFetchInfo"] = True
+
+    # Get thumbnail
+    thumbnail = soup.select_one(
+        "#item-detail > div.detail-info > div > div.col-xs-4.col-image > img"
+    )
+    if thumbnail:
+        result["thumbnail"] = thumbnail.get("src")
+
+    # Get status
+    status = soup.select_one(
+        "#item-detail > div.detail-info > div > div.col-xs-8.col-info > ul > li.status.row > p.col-xs-8"
+    )
+    if status:
+        result["status"] = status.text.strip()
+
+    # Get another name
+    another_name = soup.select_one(
+        "#item-detail > div.detail-info > div > div.col-xs-8.col-info > ul > li.othername.row > h2"
+    )
+    if another_name:
+        result["anotherName"] = another_name.text.strip().split(";")
+
+    # Get genre
+    genre = soup.select_one(
+        "#item-detail > div.detail-info > div > div.col-xs-8.col-info > ul > li.kind.row > p.col-xs-8"
+    )
+    if genre:
+        result["genre"] = {}
+        for link in genre.find_all("a"):
+            result["genre"][link.text.strip()] = link.get("href", "")
+
+    # Get content
+    content = soup.select_one("#item-detail > div.detail-content > p")
+    if content:
+        result["content"] = content.text.strip()
+
+    # Get author
+    author = soup.select_one(
+        "#item-detail > div.detail-info > div > div.col-xs-8.col-info > ul > li.author.row > p.col-xs-8"
+    )
+    if author:
+        result["author"] = {}
+        a_element = author.find("a")
+        if a_element:
+            for link in a_element.find_all("a"):
+                result["author"][link.text.strip()] = link.get("href", "")
+        else:
+            for author_text in author.text.strip().split(";"):
+                result["author"][author_text.strip()] = ""
+
+    print(result)
+
+    for key, value in result.items():
+        if isinstance(value, dict):
+            result[key] = Json(value)
+
+    return result
