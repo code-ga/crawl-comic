27a4cda4b72c3088e5686b0dc0b963e1d19089d6
code-ga <tritranduc11@gmail.com>
2024-03-22 20:31:16 +0700

    add asyncio.gather

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import aiohttp
 from dotenv import load_dotenv
 from prisma import Prisma
 from prisma.models import Comic
+from prisma.models import Html
 import os
 
 from modules.blogtruyenmoi import parseComicHtmlPage as parseComicHtmlPage_blogtruyenmoi
@@ -34,13 +35,38 @@ async def parseComicHtmlPage(html: str, comic_in_db: Prisma.comic):
         comic = parseComicHtmlPage_blogtruyenmoi(html, comic_in_db)
     elif "nettruyen" in comic_in_db.url:
         comic = parseComicHtmlPage_nettruyenff(html, comic_in_db)
-    updated = await Comic.prisma().update(
-        where={"id": comic_in_db.id},
-        data=comic,
-    )
-    if index and updated:
-        index.add_documents([updated])
-    print("updated comic", comic_in_db.id)
+    return comic
+
+
+async def fetching_comic(url: str, comic_in_db: Prisma.comic):
+    cachedHtml = await Html.prisma().find_unique(where={"url": {"equals": url}})
+    comic = {}
+    if cachedHtml:
+        print(f"parsing url {comic_in_db.url} - {comic_in_db.id}")
+        comic = await parseComicHtmlPage(cachedHtml.html, comic_in_db)
+        await Html.prisma().delete(where={"url": {"equals": url}})
+        await asyncio.sleep(1)
+    else:
+        async with aiohttp.ClientSession() as session:
+            print(f"fetching url {comic_in_db.url} - {comic_in_db.id}")
+            async with session.get(
+                comic_in_db.url, headers={"User-Agent": "Mozilla/5.0"}
+            ) as response:
+                if response.status == 200:
+                    print(f"parsing url {comic_in_db.url} - {comic_in_db.id}")
+                    html = await response.text()
+                    comic = await parseComicHtmlPage(html, comic_in_db)
+                    await asyncio.sleep(1)
+                elif response.status == 404 and "nettruyen" in comic_in_db.url:
+                    print("comic not found")
+                    comic = {"pythonFetchInfo": True}
+                elif response.status == 404:
+                    print("comic not found")
+                    comic = {"pythonFetchInfo": True}
+                else:
+                    print(f"failed to fetch url {comic_in_db.url} - {comic_in_db.id}")
+                    comic = {"pythonFetchInfo": True}
+    return comic if comic else {"pythonFetchInfo": True}
 
 
 async def main():
@@ -53,31 +79,15 @@ async def main():
         )
         if not comic_in_db:
             break
-        for c in comic_in_db:
-            cachedHtml = await db.html.find_unique(where={"url": {"equals": c.url}})
-            if cachedHtml:
-                print(f"parsing url {comic_in_db.url} - {comic_in_db.id}")
-                await parseComicHtmlPage(cachedHtml.html, c)
-                await db.html.delete(where={"url": {"equals": c.url}})
-                await asyncio.sleep(1)
-            else:
-                async with aiohttp.ClientSession() as session:
-                    print(f"fetching url {comic_in_db.url} - {comic_in_db.id}")
-                    async with session.get(
-                        comic_in_db.url, headers={"User-Agent": "Mozilla/5.0"}
-                    ) as response:
-                        if response.status == 404 and "nettruyen" in comic_in_db.url:
-                            print("comic not found")
-                            await db.comic.update(
-                                where={"id": comic_in_db.id},
-                                data={"pythonFetchInfo": True},
-                            )
-                            continue
-                        if response.status == 200:
-                            print(f"parsing url {comic_in_db.url} - {comic_in_db.id}")
-                            html = await response.text()
-                            await parseComicHtmlPage(html, comic_in_db)
-                            await asyncio.sleep(1)
+        for c in await asyncio.gather(*([fetching_comic(c.url, c) for c in comic_in_db])):
+            updated = await Comic.prisma().update(
+                where={"id": comic_in_db.id},
+                data=c,
+            )
+            if index and updated:
+                index.add_documents([updated])
+            print("updated comic", comic_in_db.id)
+
     await db.disconnect()
 
 
